42258180a0895fe7d2514d35fd285b9515dd1006
Dzulfiqar Ahnaf Nur Avicena <155936899+dzulfiqar890@users.noreply.github.com>
2024-09-01 12:56:50 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,7 @@
-alert('Selamat Datang :v');
+alert('Welcome To My Website');
+const menuIcon = document.getElementById("menu-icon");
+const navbar = document.getElementById("navbar");
+
+menuIcon.addEventListener("click", () => {
+  navbar.style.display = navbar.style.display === "none" ? "block" : "none";
+});
